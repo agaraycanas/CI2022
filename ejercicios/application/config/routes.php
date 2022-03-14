fd60ece10539853e67d879d51edfdeafd9e0b31f
Alberto Garay <alberto@loquesea>
2022-03-14 09:35:50 +0100

    routes arreglado

--- ejercicios/application/config/routes.php
+++ ejercicios/application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['init'] = 'anonymous/init';
-$route['ejercicios/t01/ej04']='ejercicios/t01/ej04/Uno';
+$route['t01/ej04']='t01/ej04/Uno';
+$route['eldelosnumeros']='t01/ej04/Uno';
