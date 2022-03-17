b0d8cb23a84ffc0fca733d77a458ac2fc6dc644d
Alberto Garay <alberto@loquesea>
2022-03-17 13:14:54 +0100

    Plantilla

---
+++ papRecuperacion/application/views/home/index.php
@@ -0,0 +1,9 @@
+<div class="container jumbotron">
+	<h1>PLANTILLA CI</h1>
+		<form action="<?=base_url()?>NADA">
+			<button>
+				Ver boletos<img src="<?=base_url()?>assets/img/lapiz.png" height="70"
+					width="170" />
+			</button>
+		</form>
+</div>
